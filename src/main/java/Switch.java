88be3eab979e5aa97ada6b233285a9a6716e2f96
“Oleh <vitali_Shulha@epam.comvitali_Shulha@epam.com>
2021-10-24 00:38:23 +0300

    Initial commit

---
+++ src/main/java/Switch.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+public class Switch {
+
+    public static void translateDayOfWeekOld(String dayOfWeek) {
+        switch (dayOfWeek) {
+            case "MONDAY":
+                System.out.println("Понедельник");
+                break;
+            case "TUESDAY":
+                System.out.println("Вторник");
+                break;
+            case "WEDNESDAY":
+                System.out.println("Среда");
+                break;
+            case "THURSDAY":
+                System.out.println("Четверг");
+                break;
+            case "FRIDAY":
+                System.out.println("Пятница");
+                break;
+            case "SATURDAY":
+                System.out.println("Суббота");
+                break;
+            case "SUNDAY":
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Day of week not found, try again with today day of week");
+                String displayName = LocalDate.now().getDayOfWeek().name();
+        }
+    }
+}
